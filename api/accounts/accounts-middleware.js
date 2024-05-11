87f3112bcdf5059794d99ea6cde875b102714b40
Katy Kane <katytheartist@gmail.com>
2024-05-11 17:14:51 -0500

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,4 +1,5 @@
 const Account = require('./accounts-model')
+const db = require('../../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
 const error = {status: 400}
@@ -21,10 +22,20 @@ if(error.message){
   // or use the Yup library (not currently installed)
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR 
-  console.log('checkAccountNameUnique middleware')
-  next()
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+  const existing = await db('accounts')
+  .where('name', req.body.name.trim())
+  .first()
+
+  if(existing){
+    next({status: 400, message: "that name is taken"})
+  } else {
+    next()
+  }
+} catch(err){
+    next(err)
+  }
 }
 
 exports.checkAccountId = async (req, res, next) => {
