d66e4c7fc2a6c6aa273a16d8cc8ec037828742e1
Katy Kane <katytheartist@gmail.com>
2024-05-10 23:35:15 -0500

    endpts wired, working and hv err handlers

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -2,26 +2,55 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+try {
+  //throw new Error('poo')
+  res.json('get accts')
+} catch(err) {
+  next(err)
+}
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+try {
+  res.json('get acct by id')
+} catch(err) {
+  next(err)
+}
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+try {
+  res.json('create acct')
+} catch(err) {
+  next(err)
+}
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+try {
+  res.json('update acct')
+} catch(err) {
+  next(err)
+}
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+try {
+  res.json('delete acct')
+} catch(err) {
+  next(err)
+}
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+  })
 })
 
 module.exports = router;
